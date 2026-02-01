785ba9a513a7bb1a5fec8ad7a180d182d930bb77
varunudata <varunudata@gmail.com>
2026-02-01 15:59:47 +0530

    fixed endpoints

--- server/server.js
+++ server/server.js
@@ -12,11 +12,11 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("Server is running");
 });
 
-app.use("/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.use("/api/db", dbRoutes);
 app.use("/api/query", queryRoutes);
